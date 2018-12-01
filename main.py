e20f5007a6491113c28466c1dcf3921ef236ad35
Alec Roques <alec.roques@gmail.com>
2018-12-01 13:08:50 -0600

    Add ridge regression calculation

--- main.py
+++ main.py
@@ -8,11 +8,23 @@ def main():
 
     print_training_data(x1, y_train)
 
-    w = np.dot(np.linalg.pinv(x_train), y_train)
+    w = linear_regression(x_train, y_train)
+    w_reg = ridge_regression(x_train, y_train, regularizer=30) 
 
-    print_regression_eq(w)
+    print_regression_eq('without reg', w)
+    print_regression_eq('with reg', w_reg)
     
-    plot_exp(x1, y_train, w)
+    plot_exp(x1, y_train, w, w_reg)
+
+def ridge_regression(x_train, y_train, regulizer):
+    I = np.identity(x_train.shape[1])
+    xTx = np.dot(x_train.T, x_train)
+    xTy = np.dot(x_train.T, y_train)
+    return np.dot(np.linalg.pinv(np.add(xTx, regulizer * I)), xTy)
+
+
+def linear_regression(x_train, y_train):
+    return np.dot(np.linalg.pinv(x_train), y_train)
 
 
 def get_x_train(x1):
@@ -29,17 +41,18 @@ def print_training_data(x1, y_train):
     print(str(np.column_stack((x1, y_train))) + '\n')
 
 
-def print_regression_eq(w):
-    print('y = {0}x + {1}'.format(round(w[1], 2), round(w[0], 2)))
+def print_regression_eq(label, w):
+    print('{0:12}: y = {1}x + {2}'.format(label, round(w[1], 2), round(w[0], 2)))
 
 
-def plot_exp(x1, y_train, w):
+def plot_exp(x1, y_train, w, w_reg):
     plt.style.use('seaborn-whitegrid')
     fig, ax = plt.subplots()
     
     ax.set(title='Ridge Regression')
     ax.scatter(x1, y_train, color='b', marker='x', label='training data')
     ax.plot(x1, line(w[1], x1, w[0]), color='g', label='regression line')
+    ax.plot(x1, line(w_reg[1], x1, w_reg[0]), color='r', label='ridge regression line')
     
     ax.legend(facecolor='w', fancybox=True, frameon=True, edgecolor='black', borderpad=1)
     plt.show()
