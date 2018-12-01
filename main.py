70eda4a2e090ae27d323e3e1f561f3ed6204d072
Alec Roques <alec.roques@gmail.com>
2018-11-30 18:13:56 -0600

    Add linear regression calculation

--- main.py
+++ main.py
@@ -2,7 +2,22 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def main():
-    print('hello wold')
+    
+    x_train = get_x_train()
+    y_train = get_y_train(x_train)
+    
+    w = np.dot(np.linalg.pinv(x_train), y_train)
+
+    print(w)
+
+def get_x_train():
+    x1 = np.random.uniform(-2, 10, 12)
+    bias = np.ones_like(x1)
+    return np.column_stack((bias, x1))
+
+
+def get_y_train(x):
+    return np.square(x) + 10
 
 
 if __name__ == '__main__':
