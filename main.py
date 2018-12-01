7a00ca24cfba8d3ced06e09868f8748778c588af
Alec Roques <alec.roques@gmail.com>
2018-11-30 20:30:26 -0600

    Add printing of training data and regression eq

--- main.py
+++ main.py
@@ -5,9 +5,21 @@ def main():
     x1 = np.random.uniform(-2, 10, 12)
     x_train = get_x_train(x1)
     y_train = get_y_train(x1)
+
+    print_training_data(x1, y_train)
+    
     w = np.dot(np.linalg.pinv(x_train), y_train)
 
-    print(w)
+    print_regression_eq(w)
+
+
+def print_training_data(x1, y_train):
+    print('(x, y) pairs:')
+    print(str(np.column_stack((x1, y_train))) + '\n')
+
+
+def print_regression_eq(w):
+    print('y = {0}x + {1}'.format(round(w[1], 2), round(w[0], 2)))
 
 
 def get_x_train(x1):
