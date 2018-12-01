952602a15608d83cd3b8eacdfbd648f35809ab8d
Alec Roques <alec.roques@gmail.com>
2018-11-30 20:06:40 -0600

    Pass x1 into get x and y train

--- main.py
+++ main.py
@@ -2,22 +2,21 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def main():
-    
-    x_train = get_x_train()
-    y_train = get_y_train(x_train)
-    
+    x1 = np.random.uniform(-2, 10, 12)
+    x_train = get_x_train(x1)
+    y_train = get_y_train(x1)
     w = np.dot(np.linalg.pinv(x_train), y_train)
 
     print(w)
 
-def get_x_train():
-    x1 = np.random.uniform(-2, 10, 12)
+
+def get_x_train(x1):
     bias = np.ones_like(x1)
     return np.column_stack((bias, x1))
 
 
-def get_y_train(x):
-    return np.square(x) + 10
+def get_y_train(x1):
+    return np.square(x1) + 10
 
 
 if __name__ == '__main__':
